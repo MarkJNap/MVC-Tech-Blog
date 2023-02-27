f5f46821248aa60675a11d9ac5bfd39104cc6526
Mark Napolitano <156marknap@gmail.omgit config --global user.name oMarkNx>
2023-02-27 16:49:19 +0800

    Added a get route for login and a get route for profile on homeRoute.js, created the userRoute post routes for creating an account, logging in and logout, created the login.js for handling the log in and sign up, created the handlebars for the homepage to display the blogs, the login handlebars, and the profile handlebars to display a users posts on their profile

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -5,7 +5,7 @@ const blogRoutes = require("./blogRoutes")
 const commentRoutes = require("./commentRoutes")
 
 router.use('/users', userRoutes);
-router.use('/blogs', blogRoutes);
-router.use('/comments', commentRoutes);
+// router.use('/blogs', blogRoutes);
+// router.use('/comments', commentRoutes);
 
 module.exports = router
--- controllers/homeRoute.js
+++ controllers/homeRoute.js
@@ -21,4 +21,28 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get("/login", (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("/profile");
+    return;
+  }
+  res.render("login");
+});
+
+router.get("/profile", withAuth, async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ["password"] },
+      include: [{ model: Blog }],
+    })
+    const user = userData.get({ plain: true })
+    res.render("profile", {
+      ...user,
+      logged_in: true,
+    })
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+
 module.exports = router;
